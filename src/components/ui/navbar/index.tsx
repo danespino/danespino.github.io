87ee995a0d21d834fd2b9d405fce316e54436735
danespino <darkness91@gmail.com>
2025-09-16 00:29:17 -0600

    Fixes TopBar bugs and adds menu styles. Minor fixes expected in the future

--- src/components/ui/navbar/index.tsx
+++ src/components/ui/navbar/index.tsx
@@ -17,18 +17,24 @@ export default function Navbar() {
 
     if(width > breakpointNav) {
         return (
-            <nav id="portfolioBar" className="w-1/12 h-full fixed left-1 justify-center max-w-[305px] border-r border-[#DBDBDB] lg:justify-start lg:w-1/12 xl:w-3/6 dark:bg-black dark:border-r dark:border-[#2B2A33]">
+            <nav id="portfolioBar" className="w-1/12 h-full fixed left-3 justify-center max-w-[280px] border-r border-[#DBDBDB] lg:justify-start lg:w-1/12 xl:w-3/6 dark:bg-black dark:border-r dark:border-[#2B2A33]">
                 <div id="tabs" className="flex flex-col justify-start mt-3">
-                    <NavItem icon="bootstrap:house-door" url="/" text="Home" className="w-11/12 px-5 py-2 rounded xl:text-nowrap lg:px-6 lg:py-3 lg:-my-1" />
-                    <NavItem icon="bootstrap:search" url="#" text="Search" className="w-11/12 px-5 py-2 rounded xl:text-nowrap lg:px-6 lg:py-3 lg:-my-1" />
-                    <NavItem icon="bootstrap:browser-safari" url="#" text="Explore" className="w-11/12 px-5 py-2 rounded xl:text-nowrap lg:px-6 lg:py-3 lg:-my-1" />
-                    <NavItem icon="bootstrap:collection-play-fill" url="#" text="Reels" className="w-11/12 px-5 py-2 rounded xl:text-nowrap lg:px-6 lg:py-3 lg:-my-1" />
-                    <NavItem icon="bootstrap:messenger" url="#" text="Messages" className="w-11/12 px-5 py-2 rounded xl:text-nowrap lg:px-6 lg:py-3 lg:-my-1" />
-                    <NavItem icon="bootstrap:heart" url="#" text="Notifications" className="w-11/12 px-5 py-2 rounded xl:text-nowrap lg:px-6 lg:py-3 lg:-my-1" />
-                    <NavItem icon="bootstrap:plus-square" url="#" text="Create" className="w-11/12 px-5 py-2 rounded xl:text-nowrap lg:px-6 lg:py-3 lg:-my-1" />
+                    <NavItem icon="bootstrap:house-door" url="/" text="Home" />
+                    <NavItem icon="bootstrap:search" url="#" text="Search" />
+                    <NavItem icon="bootstrap:browser-safari" url="#" text="Explore" />
+                    <NavItem icon="bootstrap:collection-play-fill" url="#" text="Reels" />
+                    <NavItem icon="bootstrap:messenger" url="#" text="Messages" />
+                    <NavItem icon="bootstrap:heart" url="#" text="Notifications" />
+                    <NavItem icon="bootstrap:plus-square" url="#" text="Create" />
                     <NavItem icon="bootstrap:list" url="#" text="More" navMenu>
-                        <NavItem url="#" text="Item1" className="w-full px-5 py-4 rounded xl:text-nowrap lg:px-6 lg:py-3" />
-                        <NavItem url="#" text="Item2" className="w-full px-5 py-4 rounded xl:text-nowrap lg:px-6 lg:py-3" />
+                        <NavItem icon="bootstrap:gear" url="#" text="Settings" />
+                        <NavItem icon="bootstrap:activity" url="#" text="Your Activity" />
+                        <NavItem icon="bootstrap:bookmark" url="#" text="Saved" />
+                        <NavItem icon="bootstrap:moon" url="#" text="Switch Appearence" />
+                        <NavItem icon="bootstrap:exclamation-triangle" url="#" text="Report a problem" />
+                        <hr className="w-full border-t-6 border-[#DBDBDB] dark:border-[#343434]" />
+                        <NavItem url="#" text="Switch Accounts" />
+                        <NavItem url="#" text="Log Out" />
                     </NavItem>
                 </div>
             </nav>
