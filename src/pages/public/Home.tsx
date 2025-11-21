f9aa72ec19d2163f2af3f173070fcd336bd27b95
danespino <darkness91@gmail.com>
2025-11-21 00:01:27 -0600

    Fixes row in HomePage and adds initial styles for LoginPage

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="border border-amber-400 flex flex-col items-center justify-center h-screen w-full">
+    <div className="flex flex-row items-center justify-center h-screen w-full">
       <h1 className="text-4xl font-bold">Welcome to the home page</h1>
     </div>
   )
--- src/pages/public/LoginPage.tsx
+++ src/pages/public/LoginPage.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export function LoginPage() {
     return (
-        <>
-            <h1>Login Page</h1>
-            <p>This is the login page</p>
-        </>
+        <div className="min-h-screen flex flex-col md:flex-row">
+            <div className="flex flex-col w-full items-center md:flex-row md:justify-center md:w-2/3">
+                <img src="/public/images/instantfolio_lite.svg" alt="InstantFolio Lite by Dan Espino" title="InstantFolio Lite by Dan Espino" className="w-2/3 md:w-1/3" />
+            </div>
+            <div className="flex flex-row w-full py-4 md:w-1/3 md:py-0 items-center-safe justify-around border">
+                <div className="flex flex-col w-3/4 border items-center justify-start *:w-2/3">
+                    <button className="border py-2 my-1.5">Login with Microsoft</button>
+                    <button className="border py-2 my-1.5">Login with Google</button>
+                    <button className="border py-2 my-1.5">Login with Facebook</button>
+                    <button className="border py-2 my-1.5">Login with Instagram</button>
+                </div>
+            </div>
+        </div>
     )
 }
